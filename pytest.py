fe58696a213c47c74f3cfa418b04b2cd51b2c4cb
sudoRicheek <richeekdas2001@gmail.com>
2020-03-21 00:37:19 +0530

    Update pytest.py

--- pytest.py
+++ pytest.py
@@ -4,4 +4,10 @@ import os.path
 
 def test_get_area():
 	obj = PyShape(os.path.abspath("./tests/shapes1.png"))
-	assert obj.get_area("circle", 1) == -2
+	assert obj.get_area("circle", 1) == -1
+
+def main():
+	test_get_area()
+
+if __name__ == "__main__":
+	main()
