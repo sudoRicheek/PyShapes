9fe7db19e195c632d3785d0d1b44b05085669f48
sudoRicheek <richeekdas2001@gmail.com>
2020-03-21 16:55:30 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,27 +1,24 @@
-from distutils.core import setup
-setup(
-  name = 'PyShapes',         # How you named your package folder (MyLib)
-  packages = ['PyShapes'],   # Chose the same as "name"
-  version = '1.1',      # Start with a small number and increase it with every change you make
-  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'PyShapes : A Shape detection module for Python',   # Give a short description about your library
-  author = 'Richeek Das',                   # Type in your name
-  author_email = 'richeekdas2001@gmail.com',      # Type in your E-Mail
-  url = 'https://github.com/sudoRicheek',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/sudoRicheek/PyShapes/archive/v1.1.tar.gz',    # I explain this later on
-  keywords = ['OpenCV', 'ImageProcessing', 'Utility'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
-          'numpy',
-          'opencv-python',
-      ],
-  classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
-    'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3.4',      #Specify which pyhton versions that you want to support
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
-    'Programming Language :: Python :: 3.7',
-  ],
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="PyShapes", # Replace with your own username
+    version="1.1.1",
+    author="Richeek Das",
+    author_email="richeekdas2001@gmail.com",
+    description="A shape detection module for Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sudoRicheek/PyShapes",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
 )
